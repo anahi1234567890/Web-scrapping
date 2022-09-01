3ccec12f2017fda27b08550bd01e296efe04f798
Anahi Buendia <60759729+anahi1234567890@users.noreply.github.com>
2022-09-01 14:11:45 -0500

    Update clima.py

--- clima.py
+++ clima.py
@@ -5,10 +5,14 @@ from selenium.webdriver.common.action_chains import ActionChains
 import time
 
 driver = webdriver.Chrome(executable_path=r"D:\User\Anahi\Documents\Escuela\SEMESTRE_7\SQA\chromedriver.exe")
+driver.maximize_window()
 driver.get("https://www.clima.com/")
 driver.find_element(By.XPATH,"//li[@class='m_list_countrys_mx']/a").click()
-
-driver.find_element(By.XPATH,"//div[@class='m_list_block_cities']/ul/li[21]/a").click()
-driver.find_element(By.XPATH,"//a[contains(text(),'Por horas')]").click()
-
 time.sleep(3)
+driver.find_element(By.XPATH,'//*[@id="term"]').send_keys('Querétaro')
+time.sleep(3)
+driver.find_element(By.CSS_SELECTOR, 'li:nth-child(1) .item-name').click()
+time.sleep(3)
+driver.find_element(By.XPATH,"//a[contains(text(),\'Por horas\')]").click()
+time.sleep(3)
+driver.quit()
